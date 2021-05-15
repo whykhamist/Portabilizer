684c928f513f49ed9e83638c386e809dfd3ad771
whykhamist <jhydenellana@gmail.com>
2021-05-15 11:04:47 +0800

    Moved and Renamed LoadImage method.
    Renamed LoadImage Method to LoadImageFromBytes and moved it to the Systems Library helpers.

--- Launcher/MainWindow.xaml.cs
+++ Launcher/MainWindow.xaml.cs
@@ -54,7 +54,7 @@ namespace Launcher
             }
             else if (Factory.Plugin.IconBytes.Length > 0)
             {
-                Icon = LoadImage(Factory.Plugin.IconBytes);
+                Icon = Systems.Helpers.LoadImageFromBytes(Factory.Plugin.IconBytes);
             }
 
             this.TitleBlock.Text = Configuration.Title;
@@ -218,23 +218,6 @@ namespace Launcher
 
                 RestoreRegistry(File.ReadAllText(RegFile.FullName));
             }
-        }
-        private static BitmapImage LoadImage(byte[] imageData)
-        {
-            if (imageData == null || imageData.Length == 0) return null;
-            var image = new BitmapImage();
-            using (var mem = new MemoryStream(imageData))
-            {
-                mem.Position = 0;
-                image.BeginInit();
-                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.UriSource = null;
-                image.StreamSource = mem;
-                image.EndInit();
-            }
-            image.Freeze();
-            return image;
         }
 
         private static void RestoreRegistry(string Content)
